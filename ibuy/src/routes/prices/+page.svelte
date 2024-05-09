e98ea9300a54b08afbdf25a90c951a8f92483e76
hunter <hunterjsb@gmail.com>
2024-05-08 21:44:32 -0400

    added disclaimer about pricing in table footer

--- ibuy/src/routes/prices/+page.svelte
+++ ibuy/src/routes/prices/+page.svelte
@@ -49,6 +49,17 @@
                 </tr>
             {/each}
         </tbody>
+        <tfoot>
+            <tr>
+              <td colspan="3">
+                <div style="max-height: 45px; overflow: auto;">
+                  <small>
+                    The price of a phone is determined by various factors, including the condition of the device, the associated carrier, and any outstanding financial obligations. The condition of the phone, which encompasses its physical state and functionality, directly impacts its resale value. Carrier agreements and pricing structures also influence the overall cost of the device, as carriers may offer exclusive deals or discounts on specific models. Furthermore, the amount owed on the phone plays a significant role in determining its price, as any remaining balance on a financing plan or contract can affect its resale value. Potential buyers may find a device more attractive if it is fully paid off, as they would not need to assume any additional financial responsibilities. Conversely, a substantial outstanding balance may lower the phone's price. For further clarification on these terms and their impact on the price of a phone, please contact our support team at the number provided below.
+                  </small>
+                </div>
+              </td>
+            </tr>
+          </tfoot>
     </table>
 </div>
 
