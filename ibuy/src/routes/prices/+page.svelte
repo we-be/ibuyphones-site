9a03bad2fa113f6a9c3ad5889a8ce9abb182dfb1
edfarialand <105104922+edfarialand@users.noreply.github.com>
2024-08-17 04:56:59 -0400

    Update +page.svelte

--- ibuy/src/routes/prices/+page.svelte
+++ ibuy/src/routes/prices/+page.svelte
@@ -43,35 +43,7 @@
 <!-- Responsive Container (recommended) -->
 <div class="table-container p-4">
     <!-- Native Table Element -->
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Price Minimum</th>
-                <th>Price Maximum</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each arr as row, i}
-                <tr>
-                    <td><b>{row.name}</b></td>
-                    <td>{row.priceUsed}</td>
-                    <td>{row.priceNew}</td>
-                </tr>
-            {/each}
-        </tbody>
-        <tfoot>
-            <tr>
-              <td colspan="3">
-                <div style="max-height: 45px; overflow: auto;">
-                  <small>
-                    The price of a phone is determined by various factors, including the condition of the device, the associated carrier, and any outstanding financial obligations. The condition of the phone, which encompasses its physical state and functionality, directly impacts its resale value. Carrier agreements and pricing structures also influence the overall cost of the device, as carriers may offer exclusive deals or discounts on specific models. Furthermore, the amount owed on the phone plays a significant role in determining its price, as any remaining balance on a financing plan or contract can affect its resale value. Potential buyers may find a device more attractive if it is fully paid off, as they would not need to assume any additional financial responsibilities. Conversely, a substantial outstanding balance may lower the phone's price. For further clarification on these terms and their impact on the price of a phone, please contact our support team at the number provided below.
-                  </small>
-                </div>
-              </td>
-            </tr>
-          </tfoot>
-    </table>
+    <iframe src="https://docs.google.com/spreadsheets/d/1tJQ-WdQZc91cCw0ExvPe6-lOkRuHVRbIsj4D2nNwuTo/pubhtml?gid=1216102336&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
 </div>
 
 
